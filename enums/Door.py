f365fb9a9959c3033aec2b4ff410408437d5cf2e
Anselm Kiefner <traits@anselm.kiefner.de>
2017-03-11 11:57:52 +0100

    merging..

--- enums/Door.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-IS = Enum('IS', 'closed, open, destroyed')
-BE = Enum("BE", "opened, closed, written, entered, fubared")
-HAS = Enum("HAS", "been_opened, hitpoints, been_fluxed")
--- enums/GUI.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-IS = Enum("STATE", "loading login signup playing exiting")
-CAN = Enum("CAN", "start navigate play configure")
-BE  = Enum("BE", "closed clicked")
-HAS = Enum("HAS", "clicks")
